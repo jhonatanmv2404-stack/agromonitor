fdeb81badeb580a16d60e439ea22bd176c0f177a
Mikey24 <jhonatanmv2404@gmail.com>
2026-04-25 14:00:12 -0500

    Create sw.js

---
+++ static/sw.js
@@ -0,0 +1,29 @@
+const CACHE_NAME = 'mikey24hub-v1';
+const urlsToCache = [
+  '/',
+  '/static/manifest.json',
+  '/static/icon-192.png',
+  '/static/icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  console.log('🔧 SW INSTALANDO...');
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('activate', event => {
+  console.log('✅ SW ACTIVO');
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => 
+      response || fetch(event.request)
+    )
+  );
+});
