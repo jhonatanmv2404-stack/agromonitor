13eafad7cb5b2511853dc52027c33bb9cf7ab41a
Mikey24 <jhonatanmv2404@gmail.com>
2026-04-25 11:28:53 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,23 +1,7 @@
-const CACHE_NAME = 'agromonitor-v5.2-v1';
-const urlsToCache = [
-  '/',
-  '/index_simple.html',
-  '/csv_view.html',
-  '/data',
-  '/data_full',
-  '/status'
-];
-
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-  );
+const CACHE_NAME = 'agromonitor-v5.2';
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME));
 });
-
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
-  );
+self.addEventListener('fetch', e => {
+  e.respondWith(fetch(e.request));
 });
